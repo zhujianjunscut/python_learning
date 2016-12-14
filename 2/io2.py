95e6cc3eb996df199d3c8844a1569d7e0b9a0afb
zhujianjun <zhujianjunscut@163.com>
2016-12-14 14:57:23 +0800

    first commit

---
+++ 2/io2.py
@@ -0,0 +1,4 @@
+#import json
+
+
+#test_json()
---
+++ test.py
@@ -0,0 +1,66 @@
+def test_print():
+    print "test_print"
+
+
+
+def test_dir():
+    d = {}
+    d["name"] = "zjj"
+    d["age"] = 42
+    print d
+    y = d
+    yy = d.copy()
+    print  y
+    y.clear()
+    print d,"\n", y,"\n",d, yy
+
+def test_dir_fun():
+    d = {}.fromkeys(["name","age"],"40")
+    print d
+    d2 = {}
+    print d2.get("name")
+    print  d.has_key("name1")
+    print d.items()
+    it = d.iteritems()
+    print it
+    list(it)
+    d.pop("name")
+    print d
+    d.setdefault("name","zjj")
+    print d
+    key,value = d.popitem()
+    print key, value
+    pass
+
+def test_os():
+    import os
+    print os.name
+    pass
+
+
+
+
+
+
+test_os()
+test_print()
+test_dir_fun()
+
+import logging
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
+                    datefmt='%a, %d %b %Y %H:%M:%S',
+                    filename='myapp.log',
+                    filemode='w')
+
+console = logging.StreamHandler()
+console.setLevel(logging.INFO)
+formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
+console.setFormatter(formatter)
+logging.getLogger('').addHandler(console)
+
+logging.debug('This is debug message')
+logging.info('This is info message')
+logging.warning('This is warning message')
+#test_dir()
