2cecd7236f78dd7f974ae3f66c3d45f2a7ea8b0e
zhujianjunscut <zhujianjunscut@163.com>
2016-12-15 21:33:35 +0800

    add a new file

--- process/process2.py
+++ process/process2.py
@@ -2,12 +2,12 @@ from multiprocessing import process
 import  os
 
 def run_proc(name):
-    print 'Run child process %s (%s)...' % (name, os.getpid())
+    print('Run child process %s (%s)...' % (name, os.getpid()))
 
 if __name__ == '__main__':
-    print  "parent process %s." % os.getpid()
-    p = process.Process(target=run_proc, args=('test',))
-    print "process will start"
+    print ("parent process %s." % os.getpid())
+    p = process.BaseProcess(target=run_proc, args=('test',))
+    print("process will start")
     p.start()
     p.join()
     pass
